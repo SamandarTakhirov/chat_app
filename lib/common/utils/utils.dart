39b70cfc078c6acd48f288d3c0c2242f227c412b
Soliyev909 <soliyev909@gamil.com>
2023-11-27 22:33:43 +0500

    error screen

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -8,7 +8,7 @@ void showSnackBar({required BuildContext context, required String content}) {
     SnackBar(
       content: Text(content),
     ),
-  );
+  ).close;
 }
 
 Future<File?> pickImageFromGallery(BuildContext context) async {
